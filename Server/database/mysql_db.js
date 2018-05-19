086d32a3981777ba9c4ed20ebcbeea14db9e0475
Brandon Rayson <bcray009@gmail.com>
2018-05-19 00:14:56 -0400

    Update mysql_db.js
    Removed credentials. There has been been abuse

--- Server/database/mysql_db.js
+++ Server/database/mysql_db.js
@@ -4,10 +4,10 @@ var mysql = require("mysql");
 
 exports.open_connection_default = function(){
 	var connection = mysql.createConnection({
-	  host     : 'dxbase.c0h9f8yfuf6w.us-west-2.rds.amazonaws.com',
-	  user     : 'chatadmin22',
-	  password : 'chatpassword22',
-	  database : 'firstdatabase'
+	  host     : 'xxx',
+	  user     : 'xxx',
+	  password : 'xx',
+	  database : 'xx'
 	});
 	return connection;
 };
